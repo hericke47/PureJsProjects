dbd0d1ee5848d6a64ad7379f2d4d9039624d1737
Herick Exterkoetter <herickherick47@gmail.com>
2020-09-10 06:24:29 -0300

    percentual fixo (PercentageReadBar)

---
+++ PercentageReadBar/script.js
@@ -0,0 +1,12 @@
+// criando a barra
+let bar = document.createElement("div")
+// estilo da barra
+bar.style.height = "4px"
+bar.style.width = "500px"
+bar.style.background = "#6633cc"
+bar.style.position = "fixed"
+bar.style.top = "0"
+bar.style.left = "0"
+bar.style.zIndex = "9999"
+
+document.body.append(bar);
