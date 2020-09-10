c089cf4130b0e57176fd36ba700b25c99bb28b8d
Herick Exterkoetter <herickherick47@gmail.com>
2020-09-10 05:18:47 -0300

    criando as cores iniciais e o darkmode (DarkMode)

---
+++ DarkMode/scripts.js
@@ -0,0 +1,29 @@
+const html = document.querySelector("html");
+const checkbox = document.querySelector("input[name=theme]")
+
+const getStyle = (element, style) =>
+    window
+      .getComputedStyle(element)
+      .getPropertyValue(style),
+
+const initialColors = {
+  bg: getStyle(html, "--bg"),
+  bgPanel: getStyle(html, "--bg-panel"),
+  colorHeadings: getStyle(html, "--color-headings"),
+  colorText: getStyle(html, "--color-tetx"),
+}
+
+const darkMode = {
+  bg: "#333333",
+  bgPanel: "#434343",
+  colorHeadings: "#3664FF",
+  colorText: "#B5B5B5"
+}
+
+const changeColors = (colors) => {
+
+}
+
+checkbox.addEventListener("change", ({ target }) => {
+  target.checked ? changeColors(darkMode) : changeColors(initialColors)
+})
